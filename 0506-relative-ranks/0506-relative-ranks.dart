a9f2cacc43e105c6f8965546e0540d178b91f4b4
Adarsh00700 <adarshpvarun@gmail.com>
2026-05-20 18:22:53 +0530

    Time: 32 ms (0%), Space: 157.6 MB (0%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.dart
@@ -0,0 +1,26 @@
+class Solution {
+  List<String> findRelativeRanks(List<int> score) {
+    List<int> athletScore = List.from(score);
+    athletScore.sort((a, b) => b.compareTo(a));
+    
+    Map<int, int> rank = {};
+    for (int i = 0; i < athletScore.length; i++) {
+      rank[athletScore[i]] = i + 1;
+    }
+
+    List<String> res = [];
+    for (int i = 0; i < score.length; i++) {
+      if (rank[score[i]] == 1) {
+        res.add("Gold Medal");
+      } else if (rank[score[i]] == 2) {
+        res.add("Silver Medal");
+      } else if (rank[score[i]] == 3) {
+        res.add("Bronze Medal");
+      } else {
+        res.add(rank[score[i]].toString());
+      }
+    }
+
+    return res;
+  }
+}
