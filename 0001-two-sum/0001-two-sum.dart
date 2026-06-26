e44f6100ee6725b0c026cd1405005ef34b11f601
ADARSH <adarshpvarun@gmail.com>
2026-06-26 22:00:21 +0530

    Time: 59 ms (40.09%), Space: 151 MB (28.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.dart
+++ 0001-two-sum/0001-two-sum.dart
@@ -1,12 +1,15 @@
 class Solution {
-List<int> twoSum(List<int> nums, int target) {
-  for (int i = 0; i < nums.length; i++) {
-    for (int j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) {
-        return [i, j];
+  
+  List<int> twoSum(List<int> numbers, int target) {
+    int n = numbers.length;
+    for (var i = 0; i < n - 1; i++) {
+   for (var j = i + 1; j < n; j++) {
+        if (numbers[i] + numbers[j] == target) {
+             return [i, j];
+        }
       }
     }
+
+    return [];
   }
-  return [];
-}
 }
